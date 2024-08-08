b1e08bad36cbd6993b024d1c99ce1762953a7958
RohithEBD2 <rohithkumar7996@gmail.com>
2024-08-08 22:36:51 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -4,10 +4,15 @@
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    
+    if (celcius > 200) // Simulate a failure for high temperatures
+    {
+        return 500;
+    }
     return 200;
 }
 
@@ -26,6 +31,7 @@ void alertInCelcius(float farenheit) {
 int main() {
     alertInCelcius(400.5);
     alertInCelcius(303.6);
+    assert(alertFailureCount==1);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
